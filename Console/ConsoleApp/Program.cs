3ea9d1a3b8605f87a3b7fba5d0d6d4a46c9be4ae
Artem Sergeev <artem.sergeev@aliexpress.ru>
2021-12-13 07:25:34 +0300

    +

--- Console/ConsoleApp/Program.cs
+++ Console/ConsoleApp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using ConsoleApp.Models.UniqueModels;
 
 namespace ConsoleApp
 {
@@ -8,119 +7,79 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            // var n = 5;
-            // var intArray = new int[5];
-            // FillArrayWithRandomNumbers(n, intArray);
-            // DisplayArray(intArray);
-            var array = new[,]{{10, 5, 4, 7}, {12, 15, 20, 8}, {9, 3, 88, 25}, {50, 60, 70, 80}}; //TwoDimensionArrayHelper.Create2DArray(3, 3);
-            TwoDimensionArrayHelper.Display2DArray(array);
-            //TwoDimensionArrayHelper.Exercise41(array);
-            //TwoDimensionArrayHelper.Exercise51(array);
-            TwoDimensionArrayHelper.Exercise54(array);
+            var n = 7;
+            var array = CreateRandomArray(n);
+            DisplayArray(array);
 
-            //Exercise19(n, intArray);
-            //Exercise18(n, intArray);
-            //Exercise9And10(intArray, n);
-            //Exercise13(n, intArray);
+            //1, 2, 4, 5
+            //k = 2, number = 100
+            //1, 2, 100, 4, 5
+            
+            //Exercise3(n, array);
+            //Exercise13(n, array);
+            //Exercise12(array);
         }
 
-        private static void Exercise19(int n, int[] intArray)
+        private static void Exercise3(int n, int[] array)
         {
-            //2, 2, 5, 4, 7
-            var flag = true;
+            var k = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] % 3 == 0)
+                {
+                    k++;
+                }
+            }
+
+            Console.WriteLine(k);
+        }
+
+        private static void Exercise13(int n, int[] array)
+        {
+            var k = 3;
+            var newArray = new int[n - 1];
             for (int i = 0; i < n - 1; i++)
             {
-                if (intArray[i] <= intArray[i + 1])
-                {
-                    continue;
-                }
-                else
-                {
-                    flag = false;
-                }
-            }
-
-            Console.WriteLine(flag);
-        }
-
-        private static void Exercise18(int n, int[] intArray)
-        {
-            var elementToInsert = Console.ReadLine();
-            var k = 2;
-            var newArray = new int[n + 1];
-            for (var i = 0; i < n + 1; i++)
-            {
                 if (i < k)
                 {
-                    newArray[i] = intArray[i];
+                    newArray[i] = array[i];
                 }
                 else
                 {
-                    if (i == k)
-                    {
-                        newArray[i] = int.Parse(elementToInsert);
-                    }
-                    else
-                    {
-                        newArray[i] = intArray[i - 1];
-                    }
+                    newArray[i] = array[i + 1];
                 }
             }
 
             DisplayArray(newArray);
         }
 
-        private static void Exercise9And10(int[] intArray, int n)
+        private static void Exercise12(int[] array)
         {
-            var max = -1;
-            var maxIndex = -1;
-            for (var i = 0; i < n; i++)
-            {
-                if (intArray[i] > max)
-                {
-                    max = intArray[i];
-                    maxIndex = i;
-                }
-            }
+            var k = 3;
+            var m = 5;
 
-            Console.WriteLine(max);
-            Console.WriteLine(maxIndex);
+            var c = array[k];
+            array[k] = array[m];
+            array[m] = c;
+
+            DisplayArray(array);
         }
 
-        private static void Exercise13(int n, int[] intArray)
+        static void DisplayArray(IEnumerable<int> array)
         {
-            var newArray = new int[n - 1];
-            var k = 2;
-            // 1, 2, 5, 6, 8
-            for (var i = 0; i < n - 1; i++)
-            {
-                if (i < k)
-                {
-                    newArray[i] = intArray[i];
-                }
-                else
-                {
-                    newArray[i] = intArray[i + 1];
-                }
-            }
-
-            DisplayArray(newArray);
-            // 1, 2, 6, 8
+            Console.WriteLine(string.Join(", ", array));
         }
 
-        private static void FillArrayWithRandomNumbers(int n, int[] intArray)
+        static int[] CreateRandomArray(int n)
         {
+            var array = new int[n];
             var random = new Random();
-            for (var i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
-                var randomNumber = random.Next(10, 100);
-                intArray[i] = randomNumber;
+                array[i] = random.Next(10, 100);
             }
-        }
 
-        private static void DisplayArray(int[] intArray)
-        {
-            Console.WriteLine(string.Join(", ", intArray));
+            return array;
         }
     }
 }
